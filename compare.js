11f9d773d97f26cc749a3c9ee2b51a9d5776616a
mozaherulhasan <mozaherulhasan95@gmail.com>
2023-02-27 00:24:52 +0600

    five video done

---
+++ compare.js
@@ -0,0 +1,8 @@
+const first = 2;
+const second = 2;
+if (first === second) {
+    console.log('they are same');
+}
+else{
+    console.log('different');
+}
---
+++ loop.js
@@ -0,0 +1,18 @@
+const numbers = [23, 34, 45, 43, 43];
+for(const number of numbers){
+    // console.log(number);
+}
+
+const bottle = { color: 'yellow', price: 50, isCleaned: true, capacity: 2};
+
+// const keys = object.keys(bottle);
+// console.log(keys);
+for(const key of keys){
+    // console.log(key);
+}
+
+
+// for in loop
+for( const key in bottle){
+    console.log(key);
+}
