342e7c6c549cc86a8e5dc9704d64268953fcf056
mozaherulhasan <mozaherulhasan95@gmail.com>
2023-02-26 23:40:40 +0600

    third video done

---
+++ freeze.js
@@ -0,0 +1,7 @@
+const bottle = { color: 'yellow', price: 50, isCleaned: true, capacity: 2};
+const keys = object.keys(bottle);
+// console.log(keys);
+const values = object.values(bottle);
+// console.log(values);
+const pyaar = object.entries(bottle);
+// console.log(pyaar);
